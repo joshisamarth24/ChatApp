d2fb974e22aa3f2d47bb496145a2e8961f952347
Samarth Joshi <samarth.21ug2002@iiitranchi.ac.in>
2024-04-18 08:36:27 +0530

    socket

--- frontend/src/Context/SocketContext.jsx
+++ frontend/src/Context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(user){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://chatapp-iyi3.onrender.com",{
                 query: {
                     userId: user._id
                 }
